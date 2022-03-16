3477bdb41fe552bd85345f8d2610bc601355c35a
Phung Huynh Xuan <phunghx2@fpt.com.vn>
2022-03-16 20:07:13 +0700

    Get

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -11,6 +11,7 @@ namespace TestAPI
     {
         private static HttpClient client;
         private static string IP = "https://52.0.7.90:9200";
+        private static string authen = "elastic:GSbLU=X63NL6YRUGDVWF";
 
         public static string ToJsonString(this JsonDocument jdoc)
         {
@@ -35,7 +36,7 @@ namespace TestAPI
                     };
 
                 ElasticSearch.client = new HttpClient(handler);
-                var byteArray = Encoding.ASCII.GetBytes("elastic:GSbLU=X63NL6YRUGDVWF");
+                var byteArray = Encoding.ASCII.GetBytes($"{ElasticSearch.authen}");
                 ElasticSearch.client.DefaultRequestHeaders.Authorization =
                     new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
                     Convert.ToBase64String(byteArray));
@@ -57,23 +58,111 @@ namespace TestAPI
             var responseString = await response.Content.ReadAsStringAsync();
             return (responseString);
         }
+        public static async System.Threading.Tasks.Task<string> postDataAsyncMulti(string[] docs)
+        {
+            string postdata = "";
+            foreach (var doc in docs)
+            {
+                using JsonDocument doc2 = JsonDocument.Parse(doc);
+                postdata += ElasticSearch.ToJsonString(doc2).Replace("\n","") + " ";
+            }
+            Console.WriteLine(postdata);
+            var content = new StringContent(postdata,
+                Encoding.ASCII, "application/json");
+
+
+            var response = await ElasticSearch.getClient().PostAsync("" +
+                 $"{ElasticSearch.IP}/logs-my_app-default/_bulk?pretty",
+                 content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return (responseString);
+        }
+        public static async System.Threading.Tasks.Task<string> getDataAsync(JsonDocument doc)
+        {
+            
+
+            var content = new StringContent(ElasticSearch.ToJsonString(doc),
+                Encoding.ASCII, "application/json");
+
+
+            var response = await ElasticSearch.getClient().PostAsync("" +
+                 $"{ElasticSearch.IP}/logs-my_app-default/_search?pretty",
+                 content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return (responseString);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
-            
 
-            string data = @"  { ""@timestamp"": ""2099-05-06T16:21:15.000Z"",
-                        ""event"": 
-                           {""original"": 
-                            ""192.0.2.42 - - [06 / May / 2099:16:21:15 + 0000] \""GET /images/bg.jpg HTTP/1.0\"" 200 24736""}}";
+
+
+            //string data = @"  { ""@timestamp"": ""2099-05-06T16:21:15.000Z"",
+            //            ""event"": 
+            //               {""original"": 
+            //                ""192.0.2.42 - - [06 / May / 2099:16:21:15 + 0000] \""GET /images/bg.jpg HTTP/1.0\"" 200 24736""}}";
+            //using JsonDocument doc = JsonDocument.Parse(data);
+
+            //var result = ElasticSearch.postDataAsync(doc);
+            //Console.WriteLine(result.Result);
+
+            //string [] data ={ @"  {""create"": { }}",
+            //             @"{ ""@timestamp"": ""2099-05-06T16:21:15.000Z"",
+            //            ""event"": 
+            //               {""original"": 
+            //                ""192.0.2.42 - - [06 / May / 2099:16:21:15 + 0000] \""GET /images/bg.jpg HTTP/1.0\"" 200 24736""}}",
+            //              @"  {""create"": { }}",
+            //             @"{ ""@timestamp"": ""2099-06-06T16:21:15.000Z"",
+            //            ""event"": 
+            //               {""original"": 
+            //                ""192.0.2.42 - - [06 / May / 2099:16:21:15 + 0000] \""GET /images/bg.jpg HTTP/1.0\"" 200 24736""}}"
+
+
+            //           };
+
+
+
+
+            //var result2 = ElasticSearch.postDataAsyncMulti(data);
+            //Console.WriteLine(result2.Result);
+
+            //string data = @"  { ""query"": {""match_all"":{}},
+            //            ""sort"": [ 
+            //               {""@timestamp"": 
+            //                ""desc""}]}";
+            //using JsonDocument doc = JsonDocument.Parse(data);
+
+            //var result = ElasticSearch.getDataAsync(doc);
+            //Console.WriteLine(result.Result);
+
+
+            string data = @"{
+                    ""query"": {
+                        ""range"": {
+                                    ""@timestamp"": {
+                                        ""gte"": ""3099-05-05"",
+                            ""lt"": ""4099-05-08""
+                                    }
+                                }
+                            },
+                      ""fields"": [
+                        ""@timestamp""
+                      ],
+                      ""_source"": false,
+                      ""sort"": [
+                        {
+                          ""@timestamp"": ""desc""
+                        }
+                      ]
+                                                }
+                    ";
             using JsonDocument doc = JsonDocument.Parse(data);
-
-            var result = ElasticSearch.postDataAsync(doc);
+            var result = ElasticSearch.getDataAsync(doc);
             Console.WriteLine(result.Result);
-            
         }
     }
 }
